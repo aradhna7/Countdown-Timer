ce3565ce1c8e97f206576aba5b53540155ae213e
aradhna7 <aradhna.7sharma@gmail.com>
2020-05-10 23:02:23 +0530

    initial commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+
+function App(){
+    return <div>
+        <Header />
+        <h1>hello</h1>
+        <Footer />
+    </div>
+}
+
+export default App;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer(){
+   
+
+
+}
+
+export default Footer;
